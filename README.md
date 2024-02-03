29245af2e94f41c0f4d4438e0e10bee6677f9d77
Pepijn98 <pepijn@vdbroek.dev>
2024-02-03 22:21:51 +0100

    Update 1.1.0

--- README.md
+++ README.md
@@ -2,15 +2,21 @@
 Clip Studio Paint Discord Rich Presence
 
 ## Prerequisites
-- NodeJS >= 8
+- NodeJS >= 18
 
 ## Usage
 - Clone this repo somewhere
-- In the repo folder do either `yarn install` or `npm install`
-- `npm install -g qckwinsvc` (This allows us to install scripts as a system service)
-- `qckwinsvc --name "ClipStudioPaint" --description "Clip Studio Paint Discord Presence" --script "C:\Path\To\Repo\index.js" --startImmediately`
+- In the repo folder do either `pnpm install` or `npm install`
+- `npm install -g qckwinsvc2` (This allows us to install scripts as a system service)
+- `qckwinsvc2 install name="CSPDiscord" description="Clip Studio Paint Discord Presence" path="C:\Path\To\Repo\index.js" now`
+
+**qckwinsvc2 is an updated version of qckwinsvc which stopped working, it does the exact same thing. When you run the command you have to accept 4 popups because it needs permissions to create a system service.**
 
 This will install the script as a windows system service and automatically detect when clip studio paint is opened. Change the script path to where you cloned it before using the command.
 
+## Uninstall system service
+All you have to do to uninstall the system service is this
+- `qckwinsvc2 uninstall name="CSPDiscord"`
+
 ## Preview
-<img src="https://b.catgirlsare.sexy/hlnU3fOb7-fA.png">
+<img src="https://kyra.vdbroek.dev/images/63f7e9585c5fb6119c0ad490/NibMc-z-g.png">
--- index.js
+++ index.js
@@ -1,41 +1,63 @@
-const client = require("discord-rich-presence")("934221567242690561");
-const exec = require("child_process").exec;
+import { exec } from "child_process";
+import { subscribe } from "wql-process-monitor";
+import RPC from "discord-rpc";
 
-// Check if CSP is already running when starting the script
-exec("tasklist", (_err, stdout) => {
-    if (stdout.includes("CLIPStudioPaint.exe")) {
-        client.updatePresence({
-            details: "Clip Studio Paint EX",
-            state: "🖊️ Drawing things",
-            startTimestamp: Date.now(),
-            largeImageKey: "large_logo",
-            instance: true
-        });
-    }
+const client_id = "934221567242690561";
+
+RPC.register(client_id);
+
+// Monitor for CSP to launch or exit
+const monitor = await subscribe({
+    creation: true,
+    deletion: true,
+    bin: {
+        filter: ["CLIPStudioPaint.exe"],
+        whitelist: true,
+    },
 });
 
-(async () => {
-    const { subscribe } = await import("wql-process-monitor/promises");
+const client = new RPC.Client({ transport: "ipc" });
 
-    // Monitor for CSP to launch or exit
-    const monitor = await subscribe({
-        filter: ["CLIPStudioPaint.exe"],
-        whitelist: true
+/** Set new activity */
+async function setActivity() {
+    client.setActivity({
+        details: "🖊️ Drawing",
+        // state: "<Put anything you like here e.g. twitter, pixiv or a description. Make sure to uncomment this line!>",
+        startTimestamp: Date.now(),
+        largeImageKey: "large_logo",
+        instance: true,
     });
+}
 
-    // Triggers when CSP openes
-    monitor.on("creation", async () => {
-        client.updatePresence({
-            details: "Clip Studio Paint EX",
-            state: "🖊️ Drawing things",
-            startTimestamp: Date.now(),
-            largeImageKey: "large_logo",
-            instance: true
-        });
-    });
+// Triggers when CSP openes
+monitor.on("creation", () => {
+    setActivity()
+        .then(() => console.log("[creation] - set activity"))
+        .catch(console.error);
+});
 
-    // Triggers when CSP closes
-    monitor.on("deletion", () => {
-        client.disconnect();
+// Triggers when CSP closes
+monitor.on("deletion", () => {
+    client
+        .clearActivity()
+        .then(() => console.log("clear activity"))
+        .catch(console.error);
+});
+
+client.on("ready", async () => {
+    console.log("ready");
+
+    // Check if CSP is already running when starting the script
+    exec("tasklist", async (_err, stdout) => {
+        if (stdout.includes("CLIPStudioPaint.exe")) {
+            setActivity()
+                .then(() => console.log("[tasklist] - set activity"))
+                .catch(console.error);
+        }
     });
-})();
+});
+
+client
+    .login({ clientId: client_id })
+    .then(() => console.log("logged in"))
+    .catch(console.error);
